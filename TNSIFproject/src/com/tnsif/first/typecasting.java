4aa2937d971a4a9c087bc158867190287441af69
Rishi <Rishi@Rishi>
2025-09-11 17:45:54 +0530

    Typecast

---
+++ TNSIFproject/src/com/tnsif/first/typecasting.java
@@ -0,0 +1,22 @@
+package com.tnsif.first;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		
+		int X = 10;
+        double Y = X;
+        System.out.println("Implicit Casting:");
+        System.out.println("Integer: " + X);
+        System.out.println("Double: " + Y);
+
+        
+        double R = 9.78;
+        int r = (int) R;
+        System.out.println("Explicit Casting:");
+        System.out.println("Double: " + R);
+        System.out.println("Integer: " + r);
+
+	}
+
+}
