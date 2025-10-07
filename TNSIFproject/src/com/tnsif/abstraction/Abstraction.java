cf0008ee8d9e7d7c086562f1abd43e1677f06377
Rishi <Rishi@Rishi>
2025-10-07 15:20:45 +0530

    Abstraction

---
+++ TNSIFproject/src/com/tnsif/abstraction/Abstraction.java
@@ -0,0 +1,20 @@
+package com.tnsif.abstraction;
+
+public abstract class Abstraction{
+
+		public abstract void show();
+		
+		public void demo()
+		{
+			System.out.println("This is method");
+		}
+}
+class Abstract1 extends Abstraction{
+	
+	public void show()
+	{
+		System.out.println("This is abstract method");
+	}
+}
+
+
---
+++ TNSIFproject/src/com/tnsif/abstraction/abstractionMain.java
@@ -0,0 +1,14 @@
+package com.tnsif.abstraction;
+
+public class abstractionMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Abstract1 ab = new Abstract1();
+			ab.show();
+			ab.demo();
+		
+
+	}
+
+}
